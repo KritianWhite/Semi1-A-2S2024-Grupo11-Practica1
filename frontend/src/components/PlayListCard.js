8f45fdaa124b0e573a0ffa04f7d0a983892bf8cb
Steven González <stevengonzalez088@gmail.com>
2024-08-29 01:57:27 -0600

    [ADD] creación de playlist

--- frontend/src/components/PlayListCard.js
+++ frontend/src/components/PlayListCard.js
@@ -1,19 +1,95 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Card, Button, Modal } from 'react-bootstrap';
 import TablaCanciones from './TablaCanciones';
+import PlayListForm from './playlist/PlayListForm.js';
+import Alertas from './Alertas.js';
 
-const PlaylistGrid = ({ playlists, fetchSongs }) => {
+const PlaylistGrid = ({ songs, fetchSongs, iduser }) => {
+  const [showForm, setShowForm] = useState(false);
+  const [showUpdatePhotoForm, setShowUpdatePhotoForm] = useState(false);
+  const [playlists, setPlaylists] = useState([]);
   const [selectedPlaylist, setSelectedPlaylist] = useState(null);
-  const [songs, setSongs] = useState([]);
 
   const handlePlaylistClick = async (playlistId) => {
     setSelectedPlaylist(playlistId);
 
-    // Enviar el idplaylist y recibir las canciones
+    // Enviar el idplaylist y actualizar las canciones
     const fetchedSongs = await fetchSongs({ idplaylist: playlistId });
-    setSongs(fetchedSongs);
+    //setSongs(fetchedSongs);
   };
-  
+
+  useEffect(() => {
+    // Obtenemos el id del usuario almacenado en el localStorage
+    let storedAuthData = JSON.parse(localStorage.getItem('authData'));
+    let storedUserId = storedAuthData.userId;
+    const fetchPlaylists = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/playlist/getall', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ iduser: storedUserId }),
+        });
+        if (!response.ok) {
+          Alertas.showToast('Error al obtener las playlists', 'error');
+          return;
+        }
+        const data = await response.json();
+        setPlaylists(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchPlaylists();
+  }, []);
+
+  const handleCreateOrUpdate = (newPlayList) => {
+    console.log(newPlayList);
+    if (selectedPlaylist) {
+      setPlaylists(
+        playlists.map((playlist) =>
+          playlist.id === selectedPlaylist.id ? { ...newPlayList } : playlist
+        )
+      );
+    } else {
+      setPlaylists([...playlists, newPlayList]);
+    }
+    setShowForm(false);
+    setSelectedPlaylist(null);
+  };
+
+  const handleUpdatePhoto = (updatedPhoto) => {
+    setPlaylists(
+      playlists.map((playlist) =>
+        playlist.id === selectedPlaylist.id
+          ? { ...playlist, url_portada: updatedPhoto }
+          : playlist
+      )
+    );
+    //actualizamos la caratura de la cancion actual
+    let playlist = { ...playlist, url_portada: updatedPhoto };
+    setShowUpdatePhotoForm(false);
+  };
+
+  const handleEdit = (playlist) => {
+    setSelectedPlaylist(playlist);
+    setShowForm(true);
+  };
+
+  const handleShowCreateForm = () => {
+    setSelectedPlaylist(null);
+    setShowForm(true);
+  };
+
+  const handleShowUpdatePhotoForm = (playlist) => {
+    setSelectedPlaylist(playlist);
+    setShowUpdatePhotoForm(true);
+  };
+
+
+
   return (
     <Container>
       {selectedPlaylist ? (
@@ -23,12 +99,22 @@ const PlaylistGrid = ({ playlists, fetchSongs }) => {
         </>
       ) : (
         <>
-          <h2 className="mb-4">PLAYLIST AGREGADAS</h2>
+          <Row className="align-items-center mb-4">
+            <Col>
+              <h2>TUS PLAYLIST</h2>
+            </Col>
+            <Col className="text-end">
+              <Button variant="primary" onClick={handleShowCreateForm} style={{ width: '20vh' }}>
+                Crear Playlist
+              </Button>
+            </Col>
+          </Row>
+
           <Row>
             {playlists.map((playlist) => (
-              <Col key={playlist.idplaylist} xs={6} md={4} lg={3} className="mb-4">
-                <Card onClick={() => handlePlaylistClick(playlist.idplaylist)} className="h-100 cursor-pointer">
-                  <Card.Img variant="top" src={playlist.portada} alt={playlist.nombre} />
+              <Col key={playlist.id} xs={6} md={4} lg={3} className="mb-4">
+                <Card onClick={() => handlePlaylistClick(playlist.id)} className="h-100 cursor-pointer">
+                  <Card.Img variant="top" src={playlist.url_portada} alt={playlist.nombre} />
                   <Card.Body>
                     <Card.Title>{playlist.nombre}</Card.Title>
                     <Card.Text>{playlist.descripcion}</Card.Text>
@@ -37,6 +123,20 @@ const PlaylistGrid = ({ playlists, fetchSongs }) => {
               </Col>
             ))}
           </Row>
+
+          <Modal show={showForm} onHide={() => setShowForm(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>{selectedPlaylist ? 'Actualizar Playlist' : 'Nueva Playlist'}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <PlayListForm
+                initialData={selectedPlaylist}
+                onSubmit={handleCreateOrUpdate}
+                onCancel={() => setShowForm(false)}
+                idUser={iduser}
+              />
+            </Modal.Body>
+          </Modal>
         </>
       )}
     </Container>
